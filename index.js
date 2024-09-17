248ca633e0134072017c7f975d9c7ce2b1e0cbf3
Kamal <kamal.raghe33@gmail.com>
2024-09-17 13:08:54 -0400

    commit

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-{/* <div id="string" >String:</div>
-    <input class='User' id="input" type="text" placeholder="Enter string" >  */}
-    const string = document.getElementById('string')
-    const re = document.getElementById('re')    
-    const array = [
-        { id: 1, age: 25 },
-        { id: 2, age: 26 },
-        { id: 3, age: 26 },
-        { id: 2, age: 27 },
-    ]
-    
-    // function StringId(){
-    //     const idArray = array.map(obj=>obj.id)
-    //     const arr = []
-    //     let IdString = ''
+const arr = [0,1,2,3,4]
+const size = 2
+const BigArray = []
 
-    //     for(let id of idArray){
-    //         if(!arr.includes(id)){
-    //             arr.push(id)
-                
-    //         }
-    //     }
-    //     for(let i = 0; i < arr.length; ++i){
-    //         if(i !== arr.length-1){
-    //             string.innerHTML += `${arr[i]}, `
-    //         }else{
-    //             string.innerHTML += `${arr[i]}  `
-    //         }
-    //     }
-    // }
-    
-    function StringId(){
-        const idArray = array.map(obj=>obj.id)
-        const arr = [...new Set(idArray)]
-        string.innerHTML = arr.join(', ')
-    }
+for(let i = 0; i < arr.length; i += size){
+    const subarray = (arr.slice(i, i + size))
+    BigArray.push(subarray)
+}
 
-    StringId()
+console.log(BigArray)
